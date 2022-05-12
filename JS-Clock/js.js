87ef269ad605dd11e1e27fbf07ed565d916c95e7
Hanefi Gülbahar <hanefigulbahar@gmail.com>
2022-05-12 18:25:05 +0300

    add minor changes

--- JS-Clock/js.js
+++ JS-Clock/js.js
@@ -1,35 +1,7 @@
-let username = prompt("Please write your name")
-let information = document.querySelector(".card-title");
-let user = "User not found"
+let arrayInfo = [1, 2, 3, 4]
 
+let info = arrayInfo.lenght
 
+console.log(info)
 
-
-
-if (username.length > 0) {
-    username = `${username[0].toUpperCase()}${username.slice(1).toLowerCase()}`
-    information.innerHTML += username;
-} else if (username.length <= 0) {
-    information.innerHTML = user
-};
-
-let country = prompt("Write your country")
-let clock = document.querySelector(".clock")
-let notCountry = "Country not found"
-
-let tarih = new Date();
-let yil = tarih.getFullYear();
-let ay = tarih.getMonth();
-let gun = tarih.getDay();
-let saat = tarih.getHours();
-let dakika = tarih.getMinutes();
-let saniye = tarih.getSeconds();
-
-
-
-if (country.length > 0) {
-    country = `${country[0].toUpperCase()}${country.slice(1).toLowerCase()}`
-    clock.innerHTML = `${country} ${tarih}`
-} else if (country.length <= 0) {
-    clock.innerHTML = notCountry
-};
+document.querySelector(`.info`).innerHTML = arrayInfo.length
